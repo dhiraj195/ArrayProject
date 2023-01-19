53032d91af7d94619c3329f1b5f37ca9a5c471b1
dhiraj <varpemahesh12@gmail.com>
2023-01-19 23:59:51 +0530

    Some Deletion

--- src/com/velocity/hdfc/kolhapur/Rajasthan.java
+++ src/com/velocity/hdfc/kolhapur/Rajasthan.java
@@ -10,22 +10,21 @@ public class Rajasthan {
 	public static void main(String[] args) {
 	LinkedHashMap lm =new LinkedHashMap<>();
 	lm.put(1,"Pranita");
-	lm.put(2, "Rahul");
+	
 	lm.put(3, "Enna");
 	
 	System.out.println(lm);
 	
 	TreeMap tm = new TreeMap();
 	tm.put(10, "Rajesh");
-	tm.put(11, "Mahesh");
+	
 	tm.put(12,"Piyush");
 	
 	System.out.println(tm);
 	
 	Hashtable ht = new Hashtable();
 	ht.put(20, "Pravin");
-	ht.put(21, "Sandesh");
-	ht.put(22, "Pooja");
+	
 	ht.put(23, "Madhuri");
 	
 	System.out.println(ht);
